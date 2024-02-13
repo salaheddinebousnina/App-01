724a815f51dc1a172146024ca99dd67914eb43de
salaheddinebousnina <159900809+salaheddinebousnina@users.noreply.github.com>
2024-02-13 16:08:43 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+// #1
+
+//function max2(a,b){
+
+
+//    if(a>b)
+//    return a
+//else
+// return b;
+
+
+//}
+
+//console.log(max2(5,8))
+//console.log(max2(15,8))
+//console.log(max2(18,18))
+
+
+// #2
+//function newlog(){
+//   console.log("bonjour");
+//}
+// #3
+// var n1=prompt("saisir un entier")
+// var n2=prompt("saisir un 2 eme entier ")
+// function nsomme(a,b){
+//    return(Number(a)+Number(b));
+//}
+// #4
+const mainDiv =document.querySelector(".main-div");
+
+function into(){
+     mainDiv.innerHTML = "INTO";
+}
+function out(){
+    mainDiv.innerHTML = "OUT";
+}
+function bgColor(){
+    mainDiv.computedStyleMap.background = "red";
+}
+
+
+
+
+
+
+
